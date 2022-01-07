295d9cfab867dd1cb95fa41396999fb36732bd9d
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2022-01-07 18:48:25 -0500

    add toggleterm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,7 @@ return packer.startup(function(use)
 	use("kyazdani42/nvim-web-devicons")
 	use("kyazdani42/nvim-tree.lua")
   use("nvim-lualine/lualine.nvim")
+  use("akinsho/toggleterm.nvim")
 
 	-- colorschemes
 	use({
