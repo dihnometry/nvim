7d7310b67a40c85c1b38a8dfa707bbddd462b308
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2022-01-07 15:18:37 -0500

    add comment, autopairs and gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,8 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs"
+  use "numToStr/Comment.nvim"
 
   -- colorschemes
   use {
@@ -77,6 +79,11 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "JoosepAlviste/nvim-ts-context-commentstring"
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
