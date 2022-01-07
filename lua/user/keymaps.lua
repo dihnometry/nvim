e426c22eb94eae4a61d5b1d9305aeff180dfd187
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2022-01-07 16:14:11 -0500

    add nvim-tree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,3 +68,6 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- Telescope
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
+-- NvimTree
+keymap("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
