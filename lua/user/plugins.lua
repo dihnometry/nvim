57edc560a96da93dce43a6369621c343aa7b9319
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2023-04-22 18:34:27 -0500

    remove alpha.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,7 +59,6 @@ return packer.startup(function(use)
     use("ahmedkhalf/project.nvim")
     use("lewis6991/impatient.nvim")
     use("lukas-reineke/indent-blankline.nvim")
-    use("goolord/alpha-nvim")
     use("antoinemadec/FixCursorHold.nvim")
 
     -- colorschemes
